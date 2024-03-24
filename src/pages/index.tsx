0efa33fc1daab40484f872cb9c0d552459f75ec7
jeongwon <wjddnj2216@naver.com>
2024-03-24 22:34:02 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,10 @@ import Main from "../components/main/Main";
 import { useState } from "react";
 
 export default function Home() {
-  const [account, setAccount] = useState("");
   return (
     <Background>
-      <Header setAccount={setAccount} />
-      <Main account={account} />
+      <Header />
+      <Main />
     </Background>
   );
 }
