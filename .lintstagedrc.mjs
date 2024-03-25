a1df6a1a4c422323e320071c66fe67157a944b49
Yeolyi <yeolyi1310@gmail.com>
2024-03-25 17:50:25 +0900

    refactor: eslint recommended 추가

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,8 +2,8 @@ const config = {
   "*.{js,ts,tsx,css}": "prettier --write",
 
   // TODO: 리팩터링 후 주석 해제
-  // "*.{js,ts,tsx}": "eslint --cache --fix",
-  // "*.{ts,tsx}": () => "tsc --noEmit",
+  "*.{js,ts,tsx}": "eslint --cache --fix",
+  "*.{ts,tsx}": () => "tsc --noEmit",
 };
 
 export default config;
