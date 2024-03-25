62085118783a80bacde95f9889fe3eda7392ab56
Yeolyi <yeolyi1310@gmail.com>
2024-03-25 17:41:40 +0900

    refactor: lint-staged 설정

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -4,96 +4,96 @@ import { useMutation } from "react-query";
 import { useTmpContext } from "@/pages/TmpContext";
 
 export default function ConnectButton() {
-    const { address, setAddress } = useTmpContext();
-    const [flag, setFlag] = useState(false);
+  const { address, setAddress } = useTmpContext();
+  const [flag, setFlag] = useState(false);
 
-    useEffect(() => {
-        if (
-            typeof window !== "undefined" &&
-            typeof window.ethereum !== "undefined"
-        ) {
-            window.ethereum.on("accountsChanged", connectWallet);
-            // window.ethereum.on("chainChanged", chainChanged);s
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      window.ethereum.on("accountsChanged", connectWallet);
+      // window.ethereum.on("chainChanged", chainChanged);s
+    }
+  }, []);
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      connect();
+    }
+  }, []);
+
+  // const chainChanged = async () => {
+  //   if (
+  //     typeof window !== "undefined" &&
+  //     typeof window.ethereum !== "undefined"
+  //   ) {
+  //     setAddress("");
+  //   }
+  // };
+
+  const save = useMutation({
+    mutationFn: saveAddress,
+  });
+
+  const connect = async () => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      try {
+        const res: any = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAddress(res[0]);
+
+        if (!flag) {
+          save.mutate(res[0]);
+          setFlag(true);
         }
-    }, []);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.log("Install Metamask");
+    }
+  };
 
-    useEffect(() => {
-        if (
-            typeof window !== "undefined" &&
-            typeof window.ethereum !== "undefined"
-        ) {
-            connect();
-        }
-    }, []);
+  const connectWallet = async () => {
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      try {
+        const res: any = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
 
-    // const chainChanged = async () => {
-    //   if (
-    //     typeof window !== "undefined" &&
-    //     typeof window.ethereum !== "undefined"
-    //   ) {
-    //     setAddress("");
-    //   }
-    // };
+        setAddress(res[0]);
 
-    const save = useMutation({
-        mutationFn: saveAddress,
-    });
-
-    const connect = async () => {
-        if (
-            typeof window !== "undefined" &&
-            typeof window.ethereum !== "undefined"
-        ) {
-            try {
-                const res: any = await window.ethereum.request({
-                    method: "eth_requestAccounts",
-                });
-
-                setAddress(res[0]);
-
-                if (!flag) {
-                    save.mutate(res[0]);
-                    setFlag(true);
-                }
-            } catch (err) {
-                console.error(err);
-            }
-        } else {
-            console.log("Install Metamask");
-        }
-    };
-
-    const connectWallet = async () => {
-        if (
-            typeof window !== "undefined" &&
-            typeof window.ethereum !== "undefined"
-        ) {
-            try {
-                const res: any = await window.ethereum.request({
-                    method: "eth_requestAccounts",
-                });
-
-                setAddress(res[0]);
-
-                save.mutate(res[0]);
-            } catch (err) {
-                console.error(err);
-            }
-        } else {
-            console.log("Install Metamask");
-        }
-    };
-    return (
-        <div className="relative">
-            <button
-                className="items-center py-2 px-5 rounded-full bg-[#252a39] mr-4"
-                onClick={connect}
-            >
-                <div className="flex text-white  font-montserrat  font-semibold text-[16px] leading-[24px]">
-                    <img src="metamask.png" className="h-4 w-4 mt-1 mr-2" />
-                    {address === "" ? "connect wallet" : address}
-                </div>
-            </button>
+        save.mutate(res[0]);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.log("Install Metamask");
+    }
+  };
+  return (
+    <div className="relative">
+      <button
+        className="items-center py-2 px-5 rounded-full bg-[#252a39] mr-4"
+        onClick={connect}
+      >
+        <div className="flex text-white  font-montserrat  font-semibold text-[16px] leading-[24px]">
+          <img src="metamask.png" className="h-4 w-4 mt-1 mr-2" />
+          {address === "" ? "connect wallet" : address}
         </div>
-    );
+      </button>
+    </div>
+  );
 }
--- src/pages/reward/index.tsx
+++ src/pages/reward/index.tsx
@@ -3,8 +3,6 @@ import Header from "@/components/Header";
 import AccountMain from "@/components/account/AccountMain";
 import RewardMain from "@/components/reward/RewardMain";
 
-import { useState } from "react";
-
 export default function Reward() {
   return (
     <Background>
